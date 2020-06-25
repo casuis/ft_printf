b4bcfd73cd8f3ddceafa3ae888bebdf4cb1e7404
Arthur <asimon@student.42.fr>
2020-06-25 19:12:14 +0200

    only one letter good hexa

--- conversions/convertion_i.c
+++ conversions/convertion_i.c
@@ -6,7 +6,7 @@
 /*   By: user42 <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 01:46:14 by user42            #+#    #+#             */
-/*   Updated: 2020/06/25 10:32:44 by user42           ###   ########.fr       */
+/*   Updated: 2020/06/25 19:10:34 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,31 +23,66 @@ static char		*ft_parse(char *str)
 		return ("0123456789");
 }
 
-void			ft_convertion_i(char *str)
+void			ft_convertion_octal(char *str, char *base)
 {
-	char	*base;
-	int	nb;
 	int	tmp;
 	int	i;
 
-	i = 0;
 	tmp = 0;
-	base = ft_parse(str);
-	if (*str == '0' )
-		str++;
-	if (*str == 'x')
-		str++;
-	nb = ft_atoi(str);
 	i = ft_strlen(str) - 1;
 	while (i > 0)
 	{
-		tmp += (ft_strlen(base) * i) * (*str - 48);
+		tmp += ft_power(8, i) * (*str - 48);
 		i--;
 		str++;
 	}
 	tmp += *str - 48;
 	ft_putstr(ft_itoa(tmp));
+}
 
+void			ft_convertion_hexa(char *base, char *str)
+{
+	int	i;
+	int	tmp;
+	int	y;
+
+	i = 0;
+	y = ft_strlen(str) - 1;
+	tmp = 0;
+	str = ft_lower_case(str);
+	while(y > 0)
+	{
+		while(*str != base[i])
+			i++;
+		tmp += ft_power(16, y) * i;
+		i = 0;
+		y--;
+		str++;
+	}
+	while(*str != base[i])
+		i++;
+	tmp += i;
+	ft_putstr(ft_itoa(tmp));
+}
+
+void			ft_convertion_i(char *str)
+{
+	char	*base;
+	int	nb;
+	int	i;
+
+	i = 0;
+	base = ft_parse(str);
+	if (*str == '0' )
+		str++;
+	if (*str == 'x')
+		str++;
+	if (ft_strlen(base) == 8)
+		ft_convertion_octal(str, base);
+	else if (ft_strlen(base) == 16)
+		ft_convertion_hexa(base, str);
+	else if (ft_strlen(base) == 10)
+		ft_putstr(str);
 }
 
 int		main(int argc, char **argv)
