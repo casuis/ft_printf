b59f842a7b78e09975da3ef80beb810110d22b15
Arthur <asimon@student.42.fr>
2020-06-25 10:39:54 +0200

    done octal conv

--- conversions/convertion_i.c
+++ conversions/convertion_i.c
@@ -6,11 +6,12 @@
 /*   By: user42 <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/22 01:46:14 by user42            #+#    #+#             */
-/*   Updated: 2020/06/25 09:19:19 by user42           ###   ########.fr       */
+/*   Updated: 2020/06/25 10:32:44 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../resources/utils.c"
+#include "../resources/ft_itoa.c"
 
 static char		*ft_parse(char *str)
 {
@@ -26,33 +27,34 @@ void			ft_convertion_i(char *str)
 {
 	char	*base;
 	int	nb;
-	char	*tmp;
+	int	tmp;
 	int	i;
 
 	i = 0;
+	tmp = 0;
 	base = ft_parse(str);
 	if (*str == '0' )
 		str++;
 	if (*str == 'x')
 		str++;
 	nb = ft_atoi(str);
-	i = ft_count(nb);
-	tmp = ft_create(i);
-	while (nb / ft_strlen(base) > 0)
+	i = ft_strlen(str) - 1;
+	while (i > 0)
 	{
-		tmp[i] = base[nb % ft_strlen(base)];
-		nb /= ft_strlen(base);
+		tmp += (ft_strlen(base) * i) * (*str - 48);
 		i--;
+		str++;
 	}
-	tmp[i] = (base[nb % ft_strlen(base)]);
-	ft_putstr(tmp);
-	free(tmp);
+	tmp += *str - 48;
+	ft_putstr(ft_itoa(tmp));
+
 }
 
 int		main(int argc, char **argv)
 {
+	ft_putstr("mon printf = ");
 	ft_convertion_i(argv[1]);
 	ft_putchar('\n');
-	printf("%i\n", 033);
+	printf("vrai printf = %i\n", 0xA);
 	return (0);
 }
