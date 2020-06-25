3dbbaac6f32989c68572032764a08c9580f1ebc6
Arthur <asimon@student.42.fr>
2020-06-25 10:40:30 +0200

    changes for base

--- conversions/convertion_x.c
+++ conversions/convertion_x.c
@@ -6,7 +6,7 @@
 /*   By: user42 <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/20 11:41:05 by user42            #+#    #+#             */
-/*   Updated: 2020/06/25 08:05:13 by user42           ###   ########.fr       */
+/*   Updated: 2020/06/25 09:38:07 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void		ft_convertion_x(int ptr)
 	char		*ret;
 	int		i;
 
-	i = ft_count(ptr);
+	i = ft_count(ptr, 16);
 	ret = ft_create(i);
 	base = "0123456789abcdef";
 	while(ptr / 16 > 0)
@@ -28,6 +28,7 @@ void		ft_convertion_x(int ptr)
 		i--;
 	}
 	ret[i] = base[ptr % 16];
+	ft_putstr("0x");
 	ft_putstr(ret);
 	free(ret);
 }
