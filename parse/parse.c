ae953e55f88eba6b10eac3a28abf827520fb0776
Arthur Simon <rthrsmn@gmail.com>
2020-08-06 23:58:17 +0200

    add parse dw what change

--- parse/parse.c
+++ parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 14:46:19 by asimon            #+#    #+#             */
-/*   Updated: 2020/08/06 16:38:26 by asimon           ###   ########.fr       */
+/*   Updated: 2020/08/06 23:57:09 by asimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,15 @@ int		ft_parse_convert(char *c, va_list ap)
 {
 	if (*c == 'c')
 		ft_convert_c(va_arg(ap, int));
-	else if (*c == 'x')
+	/*else if (*c == 'x')
 		ft_convert_x(va_arg(ap, int));
 	else if (*c == 'X')
 		ft_convert_xx(va_arg(ap, int));
 	else if (*c == 'u')
 		ft_convert_u(va_arg(ap, unsigned int));
-	/*else if (c == 'p')
-		ft_convert_p(va_arg(ap, void));*/
+	else if (c == 'p')
+		ft_convert_p(va_arg(ap, void));
+	*/
 	else if (*c == 'd')
 		ft_convert_d(va_arg(ap, int));
 	else if (*c == 'i')
