da4b9fd932a4d858bc1cf2924ea6ab4d4aae831d
Arthur Simon <rthrsmn@gmail.com>
2020-08-07 04:01:58 +0200

    change return in size_t

--- parse/parse.c
+++ parse/parse.c
@@ -6,7 +6,7 @@
 /*   By: asimon <asimon@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/05 14:46:19 by asimon            #+#    #+#             */
-/*   Updated: 2020/08/06 23:57:09 by asimon           ###   ########.fr       */
+/*   Updated: 2020/08/07 03:42:33 by asimon           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,27 @@
 
 int		ft_parse_convert(char *c, va_list ap)
 {
+	int			ret;
+
 	if (*c == 'c')
-		ft_convert_c(va_arg(ap, int));
-	/*else if (*c == 'x')
-		ft_convert_x(va_arg(ap, int));
+		ret = ft_convert_c(va_arg(ap, int));
+	else if (*c == 'x')
+		ret = ft_convert_x(va_arg(ap, int));
 	else if (*c == 'X')
-		ft_convert_xx(va_arg(ap, int));
+		ret = ft_convert_xx(va_arg(ap, int));
 	else if (*c == 'u')
-		ft_convert_u(va_arg(ap, unsigned int));
-	else if (c == 'p')
-		ft_convert_p(va_arg(ap, void));
-	*/
+		ret = ft_convert_u(va_arg(ap, unsigned int));
+	//else if (c == 'p')
+		///ret = ft_convert_p(va_arg(ap, void));
 	else if (*c == 'd')
-		ft_convert_d(va_arg(ap, int));
+		ret = ft_convert_d(va_arg(ap, int));
 	else if (*c == 'i')
-		ft_convert_i(va_arg(ap, char*));
+		ret = ft_convert_i(va_arg(ap, char*));
 	else if (*c == 's')
-		ft_convert_s(va_arg(ap, char*));
+		ret = ft_convert_s(va_arg(ap, char*));
 	else
-		return (0);
-	return (1);
+		return (-1);
+	return (ret);
 }
 
 void		ft_parse_flag(char *c, va_list ap)
